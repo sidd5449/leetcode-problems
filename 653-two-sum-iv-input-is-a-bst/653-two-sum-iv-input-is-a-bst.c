0df841781d958ec7d5ebdaaba08ae3075c6e9c4d
Siddhesh Thakare <76845396+sidd5449@users.noreply.github.com>
2022-07-20 00:14:56 +0530

    Time: 48 ms (28.95%), Space: 15.1 MB (72.81%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.c
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.c
@@ -8,19 +8,19 @@
  */
 
 
-bool preorder(struct TreeNode* root, int* result, int* size)
+bool preOrder(struct TreeNode* root, int* result, int* size)
 {
     if (!root)
         return;
     result[(*size)++] = root->val;
-    preorder(root->left, result, size);
-    preorder(root->right, result, size);
+    preOrder(root->left, result, size);
+    preOrder(root->right, result, size);
 }
 
 bool findTarget(struct TreeNode* root, int k){
     int result[10001];
     int size = 0;
-    preorder(root, result, &size);
+    preOrder(root, result, &size);
     
     for (int i = 0; i < size - 1; i++)
         for (int j = i + 1; j < size; j++)
