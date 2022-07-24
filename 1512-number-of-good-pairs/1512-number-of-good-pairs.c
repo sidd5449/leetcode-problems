a274395bd4c571667dd15fe84fe3cdcd6abc0a80
Siddhesh Thakare <76845396+sidd5449@users.noreply.github.com>
2022-07-24 12:28:21 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (85.59%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.c
@@ -0,0 +1,15 @@
+
+
+int numIdenticalPairs(int* nums, int numsSize){
+    int count=0;
+    for(int i=0; i<numsSize; i++){
+        for(int j=0; j<numsSize; j++){
+            if(i<j){
+               if(nums[i]==nums[j]){
+                    count++;     
+               } 
+            }
+        }
+    }
+    return count;
+}
