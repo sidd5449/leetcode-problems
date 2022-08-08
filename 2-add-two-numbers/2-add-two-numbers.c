2934ffec9bfc6779a6fe03eb70a2521fa38056a9
Siddhesh Thakare <76845396+sidd5449@users.noreply.github.com>
2022-08-08 21:53:34 +0530

    Time: 26 ms (34.81%), Space: 7.9 MB (6.48%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.c
+++ 2-add-two-numbers/2-add-two-numbers.c
@@ -1,61 +1,57 @@
-struct ListNode *createNode(int val)
-{
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode *createNode(int val){
     struct ListNode *n = malloc(sizeof(struct ListNode));
-    n->next = NULL;
+    n->next = NULL; 
     n->val = val;
     return n;
 }
 
-
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
-{
-    int carry = 0, num, reminder;
-    struct ListNode* tail = NULL;
-    struct ListNode* head;
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+    int carry = 0, num, remainder;
+    struct ListNode *tail = NULL;
+    struct ListNode *head;
     
-    while(l1 != NULL || l2 != NULL)
-    {
-        if(l1 != NULL && l2 != NULL)
-        {
-            num = l1->val + l2->val;
-            l1 = l1->next;
+    while(l1 !=NULL || l2 != NULL){
+        if(l1 != NULL && l2 != NULL){
+            num = l1-> val + l2->val;
+            l1 = l1 ->next; 
             l2 = l2->next;
+            
         }
-        else if(l1 != NULL)
-        {
+        else if(l1 != NULL){
             num = l1->val;
             l1 = l1->next;
         }
-        else
-        {
+        else{
             num = l2->val;
             l2 = l2->next;
         }
-        reminder = (num + carry) % 10; // Get the reminder
+        remainder = (num + carry) %10;
         
-        // Create new node with the valid value
-        // ------------------------------------
-        struct ListNode* node = createNode(reminder);
-        if(tail == NULL)
-        {
+        struct ListNode *node = createNode(remainder);
+        if(tail == NULL){
             head = node;
             tail = node;
-        } else {
+        }
+        else{
             tail->next = node;
             tail = node;
         }
-        carry = (num + carry) / 10;
+        carry = (num+carry)/10;
+        
         
     }
-
-    // We might have carry at the last addition
-    // ----------------------------------------
-    if(carry != 0)
-    {
-        struct ListNode* node = createNode(carry);
-        tail->next = node;
-        tail = node;
+    if(carry!=0){
+            struct ListNode *node = createNode(carry);
+            tail->next = node;
+            tail = node;
     }
-    
     return head;
 }
