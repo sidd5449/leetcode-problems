a60c22f6377e4a7a245e3e7482a97ec578bad7e9
Siddhesh Thakare <76845396+sidd5449@users.noreply.github.com>
2022-08-08 23:24:38 +0530

    Time: 188 ms (100.00%), Space: 14.4 MB (62.50%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.c
+++ 2126-destroying-asteroids/2126-destroying-asteroids.c
@@ -4,20 +4,20 @@ bool asteroidsDestroyed(int mass, int* asteroids, int asteroidsSize){
     long long massNew = mass;
     int j=0;
     int x=0;
-    int i;
+    int flag;
     while(j<asteroidsSize){
-        i=0;
+        flag=0;
         x=0;
         while(x<asteroidsSize){
             if(asteroids[x]!=0 && asteroids[x]<=massNew){
                 massNew+=asteroids[x];
-                i=1;
+                flag=1;
                 j++;
                 asteroids[x]=0;
             }
             x++;
         }
-        if(i==0){
+        if(flag==0){
             return false;
          }   
     }
